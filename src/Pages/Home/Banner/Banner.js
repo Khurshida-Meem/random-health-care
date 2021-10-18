04be1d4ead601adcc5ad726c253e36273690e849
Khurshida-Meem <meemrumi59@gmail.com>
2021-10-18 21:17:35 +0600

    added footer

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -7,15 +7,18 @@ const Banner = () => {
     return (
 
         <div className="banner banner1 d-flex justify-content-center align-items-center">
-            <div className="p-5 m-5 banner-content-bg">
-                <Fade left>
-                    <h1 className="display-3 fw-bold">Wellcome to  <br />Random Health Care</h1>
-                </Fade>
-                <Fade right>
-                    <p className="dark-text">We always provide best care of out valuable patients. <br /> We consider our patients as our family. We <br /> are always ready to take care of our family</p>
-                    <button className="pink-bg px-5 py-2 border-0 rounded text-white fw-bold">About Us</button>
-                </Fade>
+            <div className="p-5 m-3 banner-content-bg">
+                <div className="p-5 banner-content-bg">
+                    <Fade left>
+                        <h1 className="display-3 fw-bold text-white">Wellcome to  <br />Random Health Care</h1>
+                    </Fade>
+                    <Fade right>
+                        <p className="dark-text text-white">We always provide best care of out valuable patients. <br /> We consider our patients as our family. We <br /> are always ready to take care of our family</p>
+                        <button className="pink-bg px-5 py-2 border-0 rounded text-white fw-bold">About Us</button>
+                    </Fade>
+                </div>
             </div>
+
         </div>
     );
 };
--- src/Pages/Shared/Footer/Footer.js
+++ src/Pages/Shared/Footer/Footer.js
@@ -1,10 +1,38 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import logo from '../../../assets/images/logo.png'
+import './Footer.css'
 
 const Footer = () => {
     return (
-        <div>
-            i am footer
-        </div>
+        <footer className='footer py-5'>
+            <Container>
+                <div className='d-lg-flex justify-content-between align-items-center'>
+                    {/*==================== 1st part ================================*/}
+                    <div>
+                        <div className='d-flex align-items-center'>
+                            <img src={logo} alt="" width='50px' />
+                            <h2 className='ms-3 text-white'>Random Health Care Center</h2>
+                        </div>
+                        <p className=" text-white">A perfect way for your stable future.</p>
+                        <div className='d-flex'>
+                            <p className='foo-icon dark-text'><i className="fab fa-facebook"></i></p>
+                            <p className='foo-icon dark-text'><i className="fab fa-instagram-square"></i></p>
+                            <p className='foo-icon dark-text'><i className="fab fa-twitter-square"></i></p>
+                        </div>
+                    </div>
+                    {/*==================== 2nd part ================================*/}
+                    <div>
+                        <h3 className=" text-white">Contact us</h3>
+                        <h5 className=" text-white"><i className="fas dark-text fa-phone"></i> +8801943475</h5>
+                        <h5 className=" text-white"><i className="far dark-text fa-envelope"></i> university@random.edu.bd</h5>
+                        <h5 className=" text-white"><i className="fas dark-text fa-globe"></i> web@random.com</h5>
+                    </div>
+                </div>
+
+            </Container>
+            <p className='copyright text-white mb-0 pb-0'> &copy; copyright 2021 Random Health Care Center. All rights reserved</p>
+        </footer>
     );
 };
 
